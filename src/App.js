8b5bd56ec76503cd13ee8109d94add3acb59957b
erolcalisgan <erolcalisgan@hotmail.com>
2021-06-21 22:03:29 +0300

    21 Haziran Son

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
-import deneme from './pages/deneme';
+
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Navi/>
       <Container className="main">
          <Dashboard/>
-         <deneme/>
+        
       </Container>    
     </div>
   );
