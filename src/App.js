8420abab65a6feb9671daffd4f379ee23ae97066
erolcalisgan <erolcalisgan@hotmail.com>
2021-06-21 22:05:32 +0300

    Demo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
 
-
-
 function App() {
   return (
     <div className="App">
